5ba27f6203e80809972e53272b74f7a2d98ddb1e
Kosuke Muramatsu <kousuke.muramatsu@gmail.com>
2020-04-23 08:29:29 +0900

    Added - Style for MunuItem component

---
+++ src/pages/homepage/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Directory from '../../components/directory/Directory';
+import './Homepage.scss';
+
+const HomePage = () => (
+  <div className="homapage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;

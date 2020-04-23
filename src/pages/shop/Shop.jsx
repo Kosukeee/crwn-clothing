2a692611de900e884e9a223d40231de83b6f3d34
Kosuke Muramatsu <kousuke.muramatsu@gmail.com>
2020-04-23 21:50:21 +0900

    Added - Shop and CollectionPreview component

---
+++ src/pages/shop/Shop.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import SHOP_DATA from './shop_data';
+import CollectionPreview from '../../components/collection-preview/CollectionPreview';
+
+class Shop extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collections: SHOP_DATA
+    }
+  }
+
+  render() {
+    const { collections } = this.state;
+
+    return (
+      <div>{collections.map(({ id, ...otherCollectionProps }) => <CollectionPreview key={id} {...otherCollectionProps} />)}</div>
+    )
+  }
+}
+
+export default Shop;
